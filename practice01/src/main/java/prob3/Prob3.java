2c60821b174815fde706d5c1721f992b7d3c946c
hakyeonghoon1 <skyhoon8@gmail.com>
2021-09-08 10:28:55 +0900

    0908 1교시

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -5,11 +5,32 @@ import java.util.Scanner;
 public class Prob3 {
 	
 	public static void main(String[] args) {
-
+		System.out.println("숫자를 입력해주세요");
 		Scanner scanner = new Scanner(System.in);
-
+				
 		/* 코드 작성 */
 		
+		int getNum = scanner.nextInt();
+		if(getNum%2==0) {
+			int result = 0;
+			for(int i=0;i<=getNum;i++) {
+				if(i%2==0) {
+					result += i;
+				}
+			}
+			System.out.println("결과값은 : " + result);
+		}
+		else {
+			int result = 0;
+			for(int i=0;i<=getNum;i++) {
+				if(i%2==1) {
+					result += i;
+				}
+			}
+			System.out.println("결과값은 : " + result);
+		}
+		
 		scanner.close();
+		
 	}
 }
--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,5 +3,25 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
+		for(int i=1;i<=99;i++) {
+			int cnt=0;
+			for(int j=0;j<Integer.toString(i).length();j++) {
+				if(Integer.toString(i).contains("3")||Integer.toString(i).contains("6")||Integer.toString(i).contains("9")) {
+					cnt +=1;
+					System.out.println("");
+					System.out.print(Integer.toString(i).substring(0)+" ");
+					for(int k=0;k<cnt;k++) {
+						System.out.print("짝");
+					}
+					//cnt +=1;
+					//System.out.println();
+					//System.out.println(i);
+					//for(int k=0;k<cnt;k++) {
+					//	System.out.print("짝");
+					//}
+				}
+			}
+			
+		}
 	}
 }
